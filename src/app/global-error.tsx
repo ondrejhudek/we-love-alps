26a855ece717860f8af2d2c7b946767ca68b58e6
Ondrej Hudek <ondrej.hudek@gmail.com>
2023-02-03 21:13:23 +0100

    add error pages

---
+++ src/app/global-error.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+const GlobalError = () => {
+  return (
+    <html>
+      <head></head>
+      <body>
+        <h2>Something went wrong!</h2>
+        <button>Try again</button>
+      </body>
+    </html>
+  );
+};
+
+export default GlobalError;
