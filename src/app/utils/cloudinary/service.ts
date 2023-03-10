6ff64baa393468cec31a469b6b02ad84bd5cb318
Ondrej Hudek <ondrej.hudek@gmail.com>
2023-03-10 11:38:05 +0100

    fix cache + deps update

--- src/app/utils/cloudinary/service.ts
+++ src/app/utils/cloudinary/service.ts
@@ -25,39 +25,19 @@ const processImages = async (data: ImageData) =>
     }))
   );
 
-// let cachedFolders: FolderData;
-
 /**
  * Retrives all folders.
  * @returns Promise with a list of folders.
  */
 export const getFolders = async () => {
-  // if (!cachedFolders) {
-  //   cachedFolders = (await cloudinary.api.sub_folders("gallery")) as FolderData;
-  // }
-
-  // return cachedFolders;
-
   return (await cloudinary.api.sub_folders("gallery")) as FolderData;
 };
-
-// let cachedFolderThumbnails: ImageProps[];
 
 /**
  * Retrives thumbnail image for each folder. Image has to have a `gallery_thumbnail` tag.
  * @returns Promise with image resources
  */
 export const getFolderThumbnails = async () => {
-  // if (!cachedFolderThumbnails) {
-  //   const data = (await cloudinary.api.resources_by_tag("gallery_thumbnail", {
-  //     max_results: 100,
-  //   })) as ImageData;
-
-  //   cachedFolderThumbnails = await processImages(data);
-  // }
-
-  // return cachedFolderThumbnails;
-
   const data = (await cloudinary.api.resources_by_tag("gallery_thumbnail", {
     max_results: 100,
   })) as ImageData;
@@ -65,23 +45,17 @@ export const getFolderThumbnails = async () => {
   return await processImages(data);
 };
 
-let cachedImages: ImageProps[];
-
 /**
  * Retrives all images within a specified folder.
  * @param folder Folder path
  * @returns Promise with image resources
  */
 export const getImages = async (folder: string) => {
-  if (!cachedImages) {
-    const data = (await cloudinary.search
-      .expression(`resource_type:image AND folder:gallery/${folder}/*`)
-      .sort_by("public_id", "asc")
-      .max_results(100)
-      .execute()) as ImageData;
+  const data = (await cloudinary.search
+    .expression(`resource_type:image AND folder:gallery/${folder}/*`)
+    .sort_by("public_id", "asc")
+    .max_results(100)
+    .execute()) as ImageData;
 
-    cachedImages = await processImages(data);
-  }
-
-  return cachedImages;
+  return await processImages(data);
 };
