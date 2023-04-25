0ea2d628a725a4f28ecf0b412314206df62726a0
Ondrej Hudek <ondrej.hudek@gmail.com>
2023-04-25 22:13:05 +0200

    refresh photo loading component

--- src/app/(pages)/photo/[id]/loading.tsx
+++ src/app/(pages)/photo/[id]/loading.tsx
@@ -1,28 +1,7 @@
 "use client";
 
-import {
-  AspectRatio,
-  Box,
-  Card,
-  CardBody,
-  SimpleGrid,
-  Skeleton,
-} from "@chakra-ui/react";
+import LoadingSkeleton from "@/app/components/Loading";
 
-const Loading = () => (
-  <Card>
-    <CardBody p={3}>
-      <SimpleGrid columns={{ base: 1, xs: 2, sm: 3, md: 4 }} spacing="5px">
-        {[...Array(12)].map((_, i) => (
-          <Box key={`skeleton-${i}`}>
-            <AspectRatio ratio={4 / 3}>
-              <Skeleton borderRadius="none" />
-            </AspectRatio>
-          </Box>
-        ))}
-      </SimpleGrid>
-    </CardBody>
-  </Card>
-);
+const Loading = () => <LoadingSkeleton />;
 
 export default Loading;
