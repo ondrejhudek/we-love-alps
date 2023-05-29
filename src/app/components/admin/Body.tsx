d7747130866f7ac055c2707126fe7f9b810f517d
Ondrej Hudek <ondrej.hudek@gmail.com>
2023-05-29 22:37:55 +0200

    fix: sign in

--- src/app/components/admin/Body.tsx
+++ src/app/components/admin/Body.tsx
@@ -3,40 +3,68 @@
 import type { PropsWithChildren } from "react";
 import type { Session } from "next-auth";
 import { SessionProvider, signIn, useSession } from "next-auth/react";
-import { Button, Center, Skeleton } from "@chakra-ui/react";
+import {
+  Button,
+  Card,
+  CardBody,
+  Center,
+  Heading,
+  Spinner,
+} from "@chakra-ui/react";
 import { FcGoogle } from "react-icons/fc";
 
 import { AdminBody } from "@/app/components/Body";
+import Logo from "@/app/components/Logo";
+
+const SignInWrapper = ({ children }: PropsWithChildren) => (
+  <Center height="100vh" flexDirection="column">
+    {children}
+  </Center>
+);
 
 const ProtectedBody = ({ children }: PropsWithChildren) => {
   const { status } = useSession();
 
-  // const handleSignIn = () => {
-  //   signIn("google");
-  // };
-
-  if (status === "loading") return <Skeleton height={300} />;
-
-  if (status === "unauthenticated") {
+  const handleSignIn = () => {
     signIn("google");
+  };
 
-    return <Skeleton height={300} />;
-  }
-  // return (
-  //   <Center>
-  //     <Button
-  //       variant="outline"
-  //       size="lg"
-  //       leftIcon={<FcGoogle />}
-  //       fontWeight={500}
-  //       onClick={handleSignIn}
-  //     >
-  //       Sign in with Google
-  //     </Button>
-  //   </Center>
-  // );
+  if (status === "loading")
+    return (
+      <SignInWrapper>
+        <Spinner size="xl" />
+      </SignInWrapper>
+    );
 
-  return <>{children}</>;
+  if (status === "unauthenticated")
+    return (
+      <SignInWrapper>
+        <Logo iconOnly />
+
+        <Heading as="h2" my={8} fontSize="2xl">
+          Sign in to your account
+        </Heading>
+
+        <Card size="lg" width="400px" borderRadius={10}>
+          <CardBody py={8} px={10} textAlign="center">
+            <Button
+              variant="outline"
+              size="lg"
+              width="full"
+              height={14}
+              leftIcon={<FcGoogle />}
+              fontWeight={500}
+              borderRadius={8}
+              onClick={handleSignIn}
+            >
+              Google
+            </Button>
+          </CardBody>
+        </Card>
+      </SignInWrapper>
+    );
+
+  return <AdminBody>{children}</AdminBody>;
 };
 
 const Body = ({
@@ -44,9 +72,7 @@ const Body = ({
   children,
 }: PropsWithChildren<{ session: Session | null }>) => (
   <SessionProvider session={session}>
-    <AdminBody>
-      <ProtectedBody>{children}</ProtectedBody>
-    </AdminBody>
+    <ProtectedBody>{children}</ProtectedBody>
   </SessionProvider>
 );
 
