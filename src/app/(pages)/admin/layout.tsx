6e83a59ca040972854b8da069347a00e8b224550
Ondrej Hudek <ondrej.hudek@gmail.com>
2023-06-04 20:17:49 +0200

    add admin layout

--- src/app/(pages)/admin/layout.tsx
+++ src/app/(pages)/admin/layout.tsx
@@ -2,11 +2,10 @@ import type { PropsWithChildren } from "react";
 import { getServerSession, Session } from "next-auth";
 
 import authOptions from "@/app/api/auth/[...nextauth]/[...nextauth]";
-import AdminBody from "@/app/components/admin/Body";
+import { AdminBody } from "@/app/components/Body";
 
 const Layout = async ({ children }: PropsWithChildren) => {
   const session = await getServerSession<{}, Session>(authOptions);
-
   return <AdminBody session={session}>{children}</AdminBody>;
 };
 
