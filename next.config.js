2e6937bd8b3cd71460f162173684dd2b3155d343
Ondrej Hudek <ondrej.hudek@gmail.com>
2023-11-11 16:51:27 +0100

    deps update major

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
   images: {
     domains: ["res.cloudinary.com"],
     minimumCacheTTL: 3600, // 1 hour
