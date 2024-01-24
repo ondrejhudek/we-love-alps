bd6fd79386464b8a694ee6cf257842301ffc3b19
Ondrej Hudek <ondrej.hudek@gmail.com>
2024-01-24 21:44:37 +0100

    remove hotjar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { PropsWithChildren } from "react";
 import type { Metadata, Viewport } from "next";
 import { Ubuntu } from "next/font/google";
-import Script from "next/script";
 
 import AnalyticsWrapper from "@/app/components/Analytics";
 import Body from "@/app/components/Body";
@@ -34,20 +33,6 @@ const RootLayout = ({ children }: PropsWithChildren) => (
   <html lang="en" className={`${ubuntu.variable}`}>
     <body>
       <Body>{children}</Body>
-      <Script
-        id="HotJarAnalytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `(function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:3648892,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
-        }}
-      />
     </body>
     <AnalyticsWrapper />
   </html>
