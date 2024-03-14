1e2ba6bda46bde0e20ad01571960299b5dedfb26
Ondrej Hudek <ondrej.hudek@gmail.com>
2024-03-14 21:19:11 +0100

    Error refactoring

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,45 +1,7 @@
 "use client";
 
-import Link from "next/link";
-import {
-  AbsoluteCenter,
-  Alert,
-  AlertIcon,
-  AlertTitle,
-  AlertDescription,
-  Box,
-  Button,
-} from "@chakra-ui/react";
+import Error from "@/app/components/Error";
 
-const ErrorComponent = () => (
-  <Box position="relative" height="100vh">
-    <AbsoluteCenter axis="both" width={{ base: "100%", md: "auto" }}>
-      <Alert
-        status="error"
-        variant="subtle"
-        flexDirection="column"
-        mx={10}
-        py={10}
-        width={{ md: "2xl" }}
-        textAlign="center"
-        borderRadius="lg"
-      >
-        <AlertIcon boxSize="40px" mr={0} />
-
-        <AlertTitle mt={4} mb={1} fontSize="lg">
-          Něco se pokazilo!
-        </AlertTitle>
-
-        <AlertDescription maxWidth="sm">
-          Bohužel se stránka rozbila. Prosím zkus to znovu.
-        </AlertDescription>
-
-        <Button as={Link} href="/" variant="outline" colorScheme="red" mt={5}>
-          Zpět na úvod
-        </Button>
-      </Alert>
-    </AbsoluteCenter>
-  </Box>
-);
+const ErrorComponent = () => <Error />;
 
 export default ErrorComponent;
